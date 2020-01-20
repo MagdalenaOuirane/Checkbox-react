a3e93309cd1ae573036bc9b1e8c9fc6ccdaf4503
MagdalenaOuirane <magdalenaouirane@gmail.com>
2020-01-20 18:49:04 +0100

    create function component

---
+++ src/Components/NegativeMessage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NegativeMessage = () => {
+    return (
+        <p>You are not allowed to watch a movie if you are less than 16 years old</p>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+export default NegativeMessage;
