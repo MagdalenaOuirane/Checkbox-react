e7e9ed21442f0ea4b6c49b1ef5dc09c9bcf68b99
MagdalenaOuirane <magdalenaouirane@gmail.com>
2020-01-20 18:49:30 +0100

    create function component

---
+++ src/Components/PositiveMessage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const PositiveMessage = () => {
+    return (
+
+        <p>You are allowed to watch a movie.
+             You are invited!
+     </p>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default PositiveMessage;
